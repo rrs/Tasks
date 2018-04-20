32bbeffaa8f1089b89b05ca1a687c31c213ef9d7
unknown <bobblezor@gmail.com>
2018-04-20 17:53:07 +0100

    added schedule

---
+++ src/Rrs.Tasks/Schedule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace Rrs.Tasks
+{
+    public static class Schedule
+    {
+        public static void In(Action a, TimeSpan timespan, CancellationToken token = default(CancellationToken))
+        {
+            Timer timer = null;
+            timer = new Timer(_ =>
+            {
+                timer.Dispose();
+                if (token.IsCancellationRequested) return;
+                a();
+            });
+            
+            timer.Change((int)timespan.TotalMilliseconds, Timeout.Infinite);
+        }
+    }
+}
