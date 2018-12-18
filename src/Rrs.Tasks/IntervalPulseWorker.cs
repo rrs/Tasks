1135b6a26e6cb34128ad2afad3d6f84abf24849a
Robert Snipe <bobblezor@gmail.com>
2018-12-18 12:03:15 +0000

    added interval pulse worker for convenience

---
+++ src/Rrs.Tasks/IntervalPulseWorker.cs
@@ -0,0 +1,38 @@
+﻿using Rrs.Tasks.Pulsable;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Rrs.Tasks
+{
+    public sealed class IntervalPulseWorker : IDisposable
+    {
+        private PulseWorker _pulseWorker;
+        private IntervalTimer _intervalTimer;
+
+        public IntervalPulseWorker(PulseWorker pulseWorker, TimeSpan interval)
+        {
+            _pulseWorker = pulseWorker;
+            _intervalTimer = new IntervalTimer(interval, () => _pulseWorker.Pulse());
+        }
+
+        public IntervalPulseWorker(IPulsable pulsable, TimeSpan interval) : this(new PulseWorker(pulsable), interval) { }
+
+        public IntervalPulseWorker(Action<CancellationToken> pulseAction, TimeSpan interval) : this(new PulseWorker(pulseAction), interval) { }
+
+        public IntervalPulseWorker(IAsyncPulsable pulsable, TimeSpan interval) : this(new PulseWorker(pulsable), interval) { }
+
+        public IntervalPulseWorker(Func<CancellationToken, Task> pulseAction, TimeSpan interval) : this(new PulseWorker(pulseAction), interval) { }
+
+        public void Pulse()
+        {
+            _pulseWorker.Pulse();
+        }
+
+        public void Dispose()
+        {
+            _intervalTimer.Cancel();
+            _pulseWorker.Dispose();
+        }
+    }
+}
