39fa52dccbe5423711c0d4d0130a7d0eeb0a8c2f
Robert Snipe <bobblezor@gmail.com>
2019-06-18 09:51:43 +0100

    added scheduled 'task'

--- src/Rrs.Tasks/Schedule.cs
+++ src/Rrs.Tasks/Schedule.cs
@@ -5,7 +5,7 @@ namespace Rrs.Tasks
 {
     public static class Schedule
     {
-        public static void In(Action a, TimeSpan timespan, CancellationToken token = default(CancellationToken))
+        public static void In(Action a, TimeSpan timespan, CancellationToken token)
         {
             Timer timer = null;
             timer = new Timer(_ =>
@@ -15,7 +15,23 @@ namespace Rrs.Tasks
                 a();
             });
             
-            timer.Change((int)timespan.TotalMilliseconds, Timeout.Infinite);
+            timer.Change(Math.Max((int)timespan.TotalMilliseconds, 0), Timeout.Infinite);
+        }
+
+        public static ScheduledOperation In(Action a, TimeSpan timespan)
+        {
+            ScheduledOperation scheduled = null;
+            Timer timer = null;
+            timer = new Timer(_ =>
+            {
+                scheduled.Cancel();
+                a();
+            });
+            scheduled = new ScheduledOperation(timer);
+
+            timer.Change(Math.Max((int)timespan.TotalMilliseconds, 0), Timeout.Infinite);
+
+            return scheduled;
         }
     }
 }
---
+++ src/Rrs.Tasks/ScheduledTask.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Rrs.Tasks
+{
+    public class ScheduledTask
+    {
+        private readonly Action _action;
+        private volatile ScheduledOperation _scheduled;
+
+        public ScheduledTask(Action action)
+        {
+            _action = action;
+        }
+
+        public void ScheduleIn(TimeSpan timeSpan)
+        {
+            var scheduled = Interlocked.Exchange(ref _scheduled, Schedule.In(_action, timeSpan));
+            scheduled?.Cancel();
+        }
+
+        public void ScheduleIn(DateTime datetime)
+        {
+            var timeSpan = datetime - DateTime.Now;
+            var scheduled = Interlocked.Exchange(ref _scheduled, Schedule.In(_action, timeSpan));
+            scheduled?.Cancel();
+        }
+
+        public void Cancel()
+        {
+            _scheduled?.Cancel();
+        }
+    }
+}
