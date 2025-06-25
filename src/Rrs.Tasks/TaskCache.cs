94277e5b5ef405186f15821199bc0835272c0d66
Rob Snipe <rob.snipe@pointone.co.uk>
2025-06-25 16:30:44 +0100

    de typed task cache

--- src/Rrs.Tasks/TaskCache.cs
+++ src/Rrs.Tasks/TaskCache.cs
@@ -7,18 +7,25 @@ namespace Rrs.Tasks;
 /// <summary>
 /// Ensures only one task is running for a given key. All callers during execution will receive the same result. On completion the task is removed.
 /// </summary>
-public class TaskCache<TKey, TTask>
+public class TaskCache<TKey>
 {
-    private readonly ConcurrentDictionary<TKey, CachedTask<TTask>> _taskCache = new ConcurrentDictionary<TKey, CachedTask<TTask>>();
+    private readonly ConcurrentDictionary<TKey, CachedTask> _taskCache = new ConcurrentDictionary<TKey, CachedTask>();
 
-    public Task<TTask> GetOrStart(TKey key, Func<Task<TTask>> taskFactory)
+    public async Task<T> GetOrStart<T>(TKey key, Func<Task<T>> taskFactory)
+    {
+        T result = default!;
+        await GetOrStart(key, async () => result = await taskFactory());
+        return result;
+    }
+
+    public Task GetOrStart(TKey key, Func<Task> taskFactory)
     {
         var executionId = new object(); // take a unique reference to this scope
-        CachedTask<TTask> cachedTask = null;
+        CachedTask cachedTask = null;
         try
         {
             // multiple threads could create instances of cachedtask which is cheap. Only the one added to the dictionary will be used
-            cachedTask = _taskCache.GetOrAdd(key, new CachedTask<TTask>(executionId, taskFactory));
+            cachedTask = _taskCache.GetOrAdd(key, new CachedTask(executionId, taskFactory));
             // invoking the cachedtask's value starts the task, and only once
             return cachedTask.Task.Value;
         }
@@ -33,15 +40,15 @@ public class TaskCache<TKey, TTask>
     }
 
     // keeps track of a task and the context it was started in
-    private class CachedTask<T>
+    private class CachedTask
     {
         public object Id { get; }
-        public Lazy<Task<T>> Task { get; } // lazy so the task is only run once
+        public Lazy<Task> Task { get; } // lazy so the task is only run once
 
-        public CachedTask(object id, Func<Task<T>> task)
+        public CachedTask(object id, Func<Task> task)
         {
             Id = id;
-            Task = new Lazy<Task<T>>(task);
+            Task = new Lazy<Task>(task);
         }
     }
 }
